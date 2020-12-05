a80b6f7593aad39cf47fda7ea89e61bcc0eef9d2
Sean Zimbelman <Zimbiekitten@gmail.com>
2020-12-05 00:12:41 -0700

    basic tic tac toe

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ const drawBoard = () => {
     boxes.forEach((box, index) => {
         let styleString = "";
         if (index < 3) {
-            styleString += `border-bottom: 3px solid var(--green);`;
+            styleString += `border-bottom: 3px solid var(--blue);`;
         }
         if (index % 3 === 0) {
-            styleString += `border-right: 3px solid var(--green);`;
+            styleString += `border-right: 3px solid var(--blue);`;
         }
         if (index % 3 === 2) {
-            styleString += `border-left: 3px solid var(--green);`;
+            styleString += `border-left: 3px solid var(--blue);`;
         }
         if (index > 5) {
-            styleString += `border-top: 3px solid var(--green);`;
+            styleString += `border-top: 3px solid var(--blue);`;
         }
         box.style = styleString;
 
